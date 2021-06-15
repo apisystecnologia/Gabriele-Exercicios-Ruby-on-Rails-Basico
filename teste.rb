5869039c3284e8f817a4965012f9bbd81fd8349d
Gabriele Rocha <gabriele.rocha@apisys.com.br>
2021-06-15 14:46:32 -0300

    Teste

---
+++ teste.rb
@@ -0,0 +1,6 @@
+puts("digite um numero ou um operador matematico (+,-,*,/): ")
+digits = gets
+puts("digite um numero ou um operador matematico (+,-,*,/): ")
+digit = gets
+result = digits.to_i+digit.to_i
+puts result
