9f66dcd1ca235e25bf17ed5dfb84a0d0e3760995
Gabriele Rocha <gabriele.rocha@apisys.com.br>
2021-06-18 11:49:31 -0300

    Projeto Final v1.0

---
+++ Modulo 22/catalog/app/models/product.rb
@@ -0,0 +1,4 @@
+class Product < ApplicationRecord
+	has_attached_file :avatar, styles: {medium: "330x300>", thumb: "100x100>"}
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+end
